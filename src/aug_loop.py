65e2e6c8100aa9b118d31441112b9919bcd2f865
PnthrLeo <leon-1402@mail.ru>
2024-05-31 20:44:08 +0300

    add augmentation loop

---
+++ src/aug_loop.py
@@ -0,0 +1,83 @@
+from pathlib import Path
+from .workflow import modificate_workflow_v5
+import numpy as np
+from inf import ComfyRunner
+
+
+def run_augmentation(data_images_path: str, data_masks_path: str,
+                     output_path: str,
+                     number_of_inpainted_images_per_image_required: int,
+                     main_canny_weight: float, main_depth_weight: float,
+                     main_soft_edge_weight: float,
+                     main_usual_ipadapter_weight: float,
+                     main_plus_ipadapter_weight: float,
+                     main_neg_plus_ipadapter_weight: float,
+                     dataset_name: str, positive_prompt: str,
+                     negative_prompt: str, seed: int) -> None:
+    """Function to run the augmentation loop with the given parameters.
+
+    Args:
+        data_images_path (str): full path to dataset images
+        data_masks_path (str): full path to dataset masks
+        output_path (str): path to folder where the inpainted image
+            will be saved
+        number_of_inpainted_images_per_image_required (int): number of
+            inpainted images per image required
+        main_canny_weight (float): Canny Edge ControlNet weight for
+            the main model
+        main_depth_weight (float): Depth ControlNet weight for the main model
+        main_soft_edge_weight (float): Soft Edge ControlNet weight for
+            the main model
+        main_usual_ipadapter_weight (float): IPAdapter weight for
+            the main model
+        main_plus_ipadapter_weight (float): IPAdapter (plus) weight for
+            the main model
+        main_neg_plus_ipadapter_weight (float): IPAdapter (negative plus)
+            weight for the main model
+        dataset_name (str): Database name (needed for CLIP embeddings storage)
+        positive_prompt (str): Positive prompt
+        negative_prompt (str): Negative prompt
+        seed (int): Generation seed
+    """
+
+    assert Path(data_images_path).exists(), \
+        f'Path {data_images_path} does not exist'
+    assert Path(data_masks_path).exists(), \
+        f'Path {data_masks_path} does not exist'
+    assert Path(output_path).exists(), \
+        f'Path {output_path} does not exist'
+
+    data_images_path = Path(data_images_path)
+    image_list = (list(data_images_path.glob('*.jpg')) +
+                  list(data_images_path.glob('*.png')) +
+                  list(data_images_path.glob('*.jpeg')))
+
+    runner = ComfyRunner()
+
+    for img_idx, image_path in enumerate(image_list):
+        for inp_idx in range(number_of_inpainted_images_per_image_required):
+            output_name = f'{image_path.stem}-{str(inp_idx)}'
+            mask_path = str(Path(data_masks_path) / image_path.stem) + '.png'
+
+            np.random.seed(seed + (inp_idx + 1) * (img_idx + 1))
+            gen_seed = int(np.random.randint(1, 4294967294, dtype=np.int64))
+
+            pipeline = modificate_workflow_v5(str(image_path), mask_path,
+                                              data_images_path,
+                                              data_masks_path,
+                                              output_path, output_name,
+                                              main_canny_weight,
+                                              main_depth_weight,
+                                              main_soft_edge_weight,
+                                              main_usual_ipadapter_weight,
+                                              main_plus_ipadapter_weight,
+                                              main_neg_plus_ipadapter_weight,
+                                              dataset_name, positive_prompt,
+                                              negative_prompt, gen_seed)
+
+            runner.predict(
+                workflow_input=pipeline,
+                stop_server_after_completion=False
+            )
+
+    runner.stop_server()
--- src/workflow.py
+++ src/workflow.py
@@ -2,7 +2,8 @@ import json
 from pathlib import Path
 
 
-PIPELINE_PATH = 'workflows/inpainting_SDXL_v5_release_alpha_0.76_small_fixes_api.json'
+PIPELINE_PATH = 'workflows/inpainting_SDXL_v5' + \
+    '_release_alpha_0.76_small_fixes_api.json'
 
 
 def modificate_workflow_v5(image_path: str, mask_path: str,
@@ -14,7 +15,7 @@ def modificate_workflow_v5(image_path: str, mask_path: str,
                            main_plus_ipadapter_weight: float,
                            main_neg_plus_ipadapter_weight: float,
                            dataset_name: str, positive_prompt: str,
-                           negative_prompt: str, seed: int):
+                           negative_prompt: str, seed: int) -> str:
     """Function to modify the ComfyUI workflow v5 with the given parameters.
 
     Args:
@@ -40,45 +41,55 @@ def modificate_workflow_v5(image_path: str, mask_path: str,
         positive_prompt (str): Positive prompt
         negative_prompt (str): Negative prompt
         seed (int): Generation seed
+
+    Returns:
+        str: JSON string with the modified pipeline
     """
 
-    assert Path(image_path).exists(), f'Path {image_path} does not exist'
-    assert Path(mask_path).exists(), f'Path {mask_path} does not exist'
-    assert Path(data_images_path).exists(), f'Path {data_images_path} does not exist'
-    assert Path(data_masks_path).exists(), f'Path {data_masks_path} does not exist'
-    assert Path(output_path).exists(), f'Path {output_path} does not exist'
+    assert Path(image_path).exists(), \
+        f'Path {image_path} does not exist'
+    assert Path(mask_path).exists(), \
+        f'Path {mask_path} does not exist'
+    assert Path(data_images_path).exists(), \
+        f'Path {data_images_path} does not exist'
+    assert Path(data_masks_path).exists(), \
+        f'Path {data_masks_path} does not exist'
+    assert Path(output_path).exists(), \
+        f'Path {output_path} does not exist'
 
     with open(PIPELINE_PATH) as f:
         pipeline = json.load(f)
 
-        # controlnet weights "canny", "depth", "soft-edge"
-        pipeline['327']['inputs']['strength'] = main_canny_weight
-        pipeline['142']['inputs']['strength'] = main_depth_weight
-        pipeline['4']['inputs']['strength'] = main_soft_edge_weight
+    # controlnet weights "canny", "depth", "soft-edge"
+    pipeline['327']['inputs']['strength'] = main_canny_weight
+    pipeline['142']['inputs']['strength'] = main_depth_weight
+    pipeline['4']['inputs']['strength'] = main_soft_edge_weight
 
-        # ipadapter weights "usual", "plus", "negative plus"
-        pipeline['30']['inputs']['weight'] = main_usual_ipadapter_weight
-        pipeline['141']['inputs']['weight'] = main_plus_ipadapter_weight
-        pipeline['61']['inputs']['weight'] = main_neg_plus_ipadapter_weight
+    # ipadapter weights "usual", "plus", "negative plus"
+    pipeline['30']['inputs']['weight'] = main_usual_ipadapter_weight
+    pipeline['141']['inputs']['weight'] = main_plus_ipadapter_weight
+    pipeline['61']['inputs']['weight'] = main_neg_plus_ipadapter_weight
 
-        # set images loader path
-        pipeline['311']['inputs']['Text'] = data_images_path
-        # set masks loader path
-        pipeline['312']['inputs']['Text'] = data_masks_path
-        # set inpainting images saver path
-        pipeline['184']['inputs']['path'] = output_path
+    # set images loader path
+    pipeline['311']['inputs']['Text'] = data_images_path
+    # set masks loader path
+    pipeline['312']['inputs']['Text'] = data_masks_path
+    # set inpainting images saver path
+    pipeline['184']['inputs']['path'] = output_path
 
-        # database name
-        pipeline['313']['inputs']['Text'] = dataset_name
+    # database name
+    pipeline['313']['inputs']['Text'] = dataset_name
 
-        # generation seed
-        pipeline['166']['inputs']['seed'] = seed
+    # generation seed
+    pipeline['166']['inputs']['seed'] = seed
 
-        # set positive prompt
-        pipeline['167']['inputs']['text'] = positive_prompt
-        # set negative prompt
-        pipeline['168']['inputs']['text'] = negative_prompt
+    # set positive prompt
+    pipeline['167']['inputs']['text'] = positive_prompt
+    # set negative prompt
+    pipeline['168']['inputs']['text'] = negative_prompt
 
-        pipeline['309']['inputs']['Text'] = image_path
-        pipeline['310']['inputs']['Text'] = mask_path
-        pipeline['184']['inputs']['filename'] = output_name
+    pipeline['309']['inputs']['Text'] = image_path
+    pipeline['310']['inputs']['Text'] = mask_path
+    pipeline['184']['inputs']['filename'] = output_name
+
+    return json.dumps(pipeline)
