30ebc378a7247daa7402b4f826dab8b78fa0c00e
PnthrLeo <leon-1402@mail.ru>
2024-05-31 22:56:41 +0300

    fix .bat files

--- run.bat
+++ run.bat
@@ -1,11 +1,11 @@
-./venv/bin/activate.bat
+call ./venv/bin/activate.bat
 set data_images_path="/path/to/data/images"
 set data_masks_path="/path/to/data/masks"
 set output_path="/path/to/output/folder"
 set number_of_inpainted_images_per_image_required=1
 set main_canny_weight=0.5
 set main_depth_weight=0.5
-set main soft_edge_weight=0.5
+set main_soft_edge_weight=0.5
 set main_usual_ipadapter_weight=0.5
 set main_plus_ipadapter_weight=0.5
 set main_neg_plus_ipadapter_weight=0.5
--- setup.bat
+++ setup.bat
@@ -1,6 +1,11 @@
 git clone https://github.com/piyushK52/comfy-runner
-ren comfy-runner comfy_runner
-copy NUL comfy_runner/__init__.py
-python3 -m venv venv
-./venv/bin/activate.bat
-pip install -r comfy_runner/requirements.txt
+copy comfy-runner\requirements.txt requirements.txt
+copy comfy-runner\inf.py inf.py
+copy comfy-runner\constants.py constants.py
+robocopy /e comfy-runner\utils utils
+robocopy /e comfy-runner\data data
+rmdir /s /q comfy-runner
+
+python -m venv venv
+call ./venv/Scripts/activate.bat
+pip install -r requirements.txt
